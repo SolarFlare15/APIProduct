86a139bbf2a9c2cb5cdec08fdb6d4fc7fe841fa4
SolarFlare150 <brady_pelletier@yahoo.com>
2023-06-09 10:05:25 -0300

    FinalPush

--- app.js
+++ app.js
@@ -1,45 +1,83 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 
-//fetch("https://api.products.stage")
-
 const products = [
-    {
-      "id": 1,
-      "name": "Fjallraven - Foldsack No. 1 Backpack",
-      "category": "men's clothing",
-      "price": 109.95,
-    },
-  ];
+  {
+    "id": 1,
+    "name": "Fjallraven - Foldsack No. 1 Backpack",
+    "category": "men's clothing",
+    "price": 109.95,
+  },
   
+  {
+    "id": 2,
+    "name": "ProductName",
+    "category": "ProductCategory",
+    "price": 105.12,
+  },
 
-//Endpoint de la page titre
+  {
+    "id": 3,
+    "name": "TestName",
+    "category": "TestCat",
+    "price": 19.95,
+  },
+];
 
 app.get('/', (req, res) => {
-    return res.send('<html><head><title>Page Title</title></head><body>Voici la page principale</body></html>');
-  });  
+  return res.send('<html><head><title>Page Title</title></head><body>Voici la page principale</body></html>');
+});
 
-// api/product diff /api/product
-app.get("/api/product", (req, res) => { //get request
-    return res.json(products);
-  });
+// Recherche des produits par nom ou catégorie
+app.get('/api/product/search', (req, res) => {
+  const { name, category } = req.query;
 
-  // Endpoint to get product by id
+  let filteredProducts = products;
+
+  if (name) {
+    filteredProducts = filteredProducts.filter(p => p.name.includes(name));
+  }
+
+  if (category) {
+    filteredProducts = filteredProducts.filter(p => p.category === category);
+  }
+
+  if (filteredProducts.length === 0) {
+    return res.status(404).json({ error: 'No products found' });
+  }
+
+  return res.json(filteredProducts);
+});
+
+
+// Pagination des résultats
+app.get('/api/product', (req, res) => {
+  const { limit = 10, offset = 0 } = req.query;
+
+  const startIndex = parseInt(offset);
+  const endIndex = startIndex + parseInt(limit);
+
+  const paginatedProducts = products.slice(startIndex, endIndex);
+
+  return res.json(paginatedProducts);
+});
+
+// Endpoint pour obtenir un produit par son id
 app.get('/api/product/:id', (req, res) => {
   const productId = parseInt(req.params.id);
   const product = products.find(p => p.id === productId);
-  
+
   if (!product) {
     return res.status(404).json({ error: 'Product not found' });
   }
-  
+
   return res.json(product);
 });
 
-// Endpoint to create a new product
+// Endpoint pour créer un nouveau produit
 app.post('/api/product', (req, res) => {
   const { id, name, category, price } = req.body;
 
@@ -59,7 +97,7 @@ app.post('/api/product', (req, res) => {
   return res.status(201).json(newProduct);
 });
 
-//Endpoint to update a product
+// Endpoint pour mettre à jour un produit
 app.put('/api/product/:id', (req, res) => {
   const productId = parseInt(req.params.id);
   const updatedProduct = req.body;
@@ -75,7 +113,7 @@ app.put('/api/product/:id', (req, res) => {
   return res.json(products[productIndex]);
 });
 
-// Endpoint to delete a product
+// Endpoint pour supprimer un produit
 app.delete('/api/product/:id', (req, res) => {
   const productId = parseInt(req.params.id);
 
@@ -91,5 +129,5 @@ app.delete('/api/product/:id', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  return console.log(`le serveur est lancé sur le port: ${PORT}`)
-})
+  console.log(`Le serveur est lancé sur le port: ${PORT}`);
+});
